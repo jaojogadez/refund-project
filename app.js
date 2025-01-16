475a19bbdbf5251e81c95401fbc2536be41d24c5
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2025-01-16 19:16:33 -0300

    começei o projeto refund

---
+++ app.js
@@ -0,0 +1,11 @@
+/* FORM's ELEMENTS */
+const $amount = document.querySelector("#amount");
+
+$amount.oninput = () => {
+  let value = $amount.value.replace(/\D/g, "");
+  $amount.value = formatCurrencyBRL(value);
+};
+
+let formatCurrencyBRL = (value) => {
+    value = value.toLocaleString("pt-BR")
+};
