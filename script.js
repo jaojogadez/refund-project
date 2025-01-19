075203afcb7e034ea22fd81e5761b09ff3c1295d
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2025-01-19 13:00:09 -0300

    consegui converter para centavos

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const $amount = document.querySelector("#amount");
 
 $amount.oninput = () => {
   let value = $amount.value.replace(/\D/g, "");
-  value = Number(value) / 100
+  value = Number(value / 100);
   $amount.value = formatCurrencyBRL(value);
 };
 
 let formatCurrencyBRL = (value) => {
   value = value.toLocaleString("pt-BR", {
     style: "currency",
-    currency: "BRL",
-  })
-  return value
+    currency: "BRL"
+  });
+  return value;
 };
